1482939a362371ace650b2035bfaef1c687c825d
Thomas Packham <72440768+TPackham1@users.noreply.github.com>
2024-01-30 21:34:02 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+
+
+namespace CarRegSolution
+
+{
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+            string carRegAnswer = "-1";
+            int carYearAnswer = -1;
+
+
+            carRegAnswer = CarRegASW();
+
+          /*  Console.WriteLine(hasCarReg);
+
+            year = CarAge();
+            Console.WriteLine(year);*/
+
+        }
+
+
+        static string CarRegASW()
+        {
+            string temp;
+            Console.WriteLine("Does your car have a registration number? y/n");
+            string input = Console.ReadLine();
+                     
+            while (true)
+            {
+                if (input == "n")
+                {
+                    temp =  "-1";
+                    break;
+                }
+
+                else if (input == "y")
+                {
+                    temp = input;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please answer with y or n");
+                    input = Console.ReadLine();
+                }
+
+            }
+
+            return temp;
+               
+        }
+        static bool CarAge()
+        {
+
+            return false;
+
+        }
+
+       
+    }
+    } 
+
+
